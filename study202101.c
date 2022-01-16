cde8ce0c579288ba43093f984fdf14250c7cf8b7
BITEYING <96273196+Biteliuying@users.noreply.github.com>
2022-01-16 19:36:39 +0800

    first add the fire
    9*9
    mipic

---
+++ study202101.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i, j, result;
+	for (i = 9;i >= 1;i--)
+	{
+		for (j = 1;j <= i;j++)
+		{
+			result = i * j;
+			printf("%d*%d=%d\t", i, j, result);
+		}
+		printf("\n");
+	}
+	return 0;
+}
